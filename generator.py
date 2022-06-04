c310e52347acb9f4851718dcb49771e95771bed9
musaubrian <musaubrian15@gmail.com>
2022-06-04 14:47:26 +0300

    incoporated password generator

---
+++ generator.py
@@ -0,0 +1,70 @@
+import random
+import time
+
+def password_generator():
+    print("""
+****************************************
+
+           Password generator
+
+                yes: y
+                no: n
+
+****************************************
+    """)
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase = uppercase.lower()
+    digits = "1234567890"
+    symbols = " *&%$#@!+~:-<|?/ >"
+
+    password_length = int(input("Length of password: "))
+    password_amount = int(input("Number of passwords: "))
+    sym = input("Would you like to use symbols?\n~# ")
+    print()
+    lower = input("Would you like to use lowercase letters?\n~# ")
+    print()
+    nums = input("Would you like to use numbers?\n~# ")
+    print()
+
+    # use of symbols
+    if (sym == "y"):
+        syms = True
+    else:
+        syms = False
+
+    # use of lowercase letters
+    if(lower == "y"):
+        lower = True
+    else:
+        lower = False
+
+    # use of numbers
+    if(nums == "y"):
+        nums = True
+    else:
+        nums = False
+
+    # uses uppercase letters no matter what
+    upper = True
+
+    
+    # empty string where the random values are joined to
+    empty_pass = ""
+
+    if upper:
+        empty_pass += uppercase
+
+    if lower:
+        empty_pass += lowercase
+
+    if nums:
+        empty_pass += digits
+
+    if syms:
+        empty_pass += symbols
+
+    print("**"*password_length)         
+    for _ in range(password_amount):
+        generated_password = "".join(random.sample(empty_pass, password_length))
+        print(f"~# {generated_password}")
+
--- main.py
+++ main.py
@@ -1,5 +1,45 @@
+from generator import password_generator
 from database import main
+import os
+import time
 
+def clear_console():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def handle_error():
+    print("\n🤷 Unknown input‼\nStart Over?(y/n)")
+    restart = input("~# ")
+    if restart == "y":
+        clear_console()
+        select_module()
+    else:
+        print("\nSee you next time🙋")
+        time.sleep(1)
+        clear_console()
+        SystemExit()
+
+def select_module():
+    print("""
+****************************************
+     
+        What shall it be today?
+
+            database: db
+        password generator: gen
+
+****************************************        
+    """)
+
+    choices = ["db", "gen"]
+    user_prompt = input("\n~# ")
+    if user_prompt == "db":
+        clear_console()
+        main()
+    elif user_prompt == "gen":
+        clear_console()
+        password_generator()
+    elif user_prompt not in choices:
+        handle_error()
 
 if __name__ == '__main__':
-    main()
+    select_module()
