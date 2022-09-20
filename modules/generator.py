881e1c5c8bce7f1cee1ddf5f13b8458c13fa10f7
musaubrian <musaubrian15@gmail.com>
2022-09-20 21:59:39 +0300

    update: remove unused imports

--- modules/generator.py
+++ modules/generator.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """module generates random passwords"""
 import random
-import time
 
 
 class PasswordGenerator():
@@ -26,36 +25,21 @@ class PasswordGenerator():
             raise TypeError(f"{self.password_amount} has to be an integer")
         elif self.password_amount < 0 or self.password_length < 0:
             raise ValueError("Input needs to be >= 0")
-        self.use_symbols = input("Would you like to use symbols?\n~# ")
-        self.use_lower = input("Would you like to use lowercase letters?\n~# ")
-        self.use_digits = input("Would you like to use numbers?\n~# ")
         print()
 
     def start_join(self):
         """generates random vlues and joins them"""
         print("""
-****************************************
+***********************************
 
-           Password generator
+        Password generator 
 
-                yes: y
-                no: n
-
-****************************************
+***********************************
     """)
         PasswordGenerator.take_input(self)
-        if self.use_symbols == "y":
-            self.empty_string += self.symbols
-        else:
-            pass
-        if self.use_digits == "y":
-            self.empty_string += self.digits
-        else:
-            pass
-        if self.use_lower == "y":
-            self.empty_string += self.lower
-        else:
-            self.empty_string
+        self.empty_string += self.digits
+        self.empty_string += self.lower
+        self.empty_string += self.symbols
 
         print("\n", "*"*len(self.upper), "\n")
         for _ in range(self.password_amount):
