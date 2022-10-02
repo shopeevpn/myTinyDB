c865442be7bf61c6b51af88777b8bf6cd42a8eaf
musaubrian <musaubrian15@gmail.com>
2022-10-02 22:19:00 +0300

    chore: generator to issue 15 char single password

---
+++ modules/copy_to_clipboard.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""
+copies generated password to clipboard
+"""
+
--- modules/generator.py
+++ modules/generator.py
@@ -15,18 +15,6 @@ class PasswordGenerator():
         self.symbols = "*&%$#@!+~:-<|?/ >"
         self.empty_string = "" + self.upper
 
-    def take_input(self):
-        """take user input"""
-        self.password_length = int(input("Length of password: "))
-        self.password_amount = int(input("Number of passwords: "))
-        if not isinstance(self.password_length, int):
-            raise TypeError(f"{self.password_length} has to be an integer")
-        elif not isinstance(self.password_amount, int):
-            raise TypeError(f"{self.password_amount} has to be an integer")
-        elif self.password_amount < 0 or self.password_length < 0:
-            raise ValueError("Input needs to be >= 0")
-        print()
-
     def start_join(self):
         """generates random vlues and joins them"""
         print("""
@@ -36,17 +24,16 @@ class PasswordGenerator():
 
 ***********************************
     """)
-        PasswordGenerator.take_input(self)
+
         self.empty_string += self.digits
         self.empty_string += self.lower
         self.empty_string += self.symbols
+        self.password_length = 15
 
-        print("\n", "*"*len(self.upper), "\n")
-        for _ in range(self.password_amount):
-            self.generated_password = "".join(random.sample(
-                self.empty_string, self.password_length
-                ))
-            print(f"~# {self.generated_password}")
+        self.generated_password = "".join(random.sample(
+            self.empty_string, self.password_length
+            ))
+        print(f"~# {self.generated_password}")
 
 
 start_generator = PasswordGenerator()
