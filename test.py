a240d77e1874feba84a109649340265b43bfec0e
musaubrian <musaubrian15@gmail.com>
2022-10-22 14:25:45 +0300

    Refactor: remove test script

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from modules.handle_files import encrypt_file, decrypt_file, generate_key, to_key
-from modules.database import full_path
-
-if __name__ == "__main__":
-    #    generate_key()
-#        encrypt_file(db_path=full_path, key_path=to_key)
-    decrypt_file(db_path=full_path, key_path=to_key)
